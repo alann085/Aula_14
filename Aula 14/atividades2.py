a14010bb0a91262f2c9d29c6455ffd52daee95d8
alann085 <142846421+alann085@users.noreply.github.com>
2023-08-22 20:45:14 -0300

    Update atividades2.py

--- Aula 14/atividades2.py
+++ Aula 14/atividades2.py
@@ -2,7 +2,7 @@ lista = [2,3,1,4,5,6,8,7,9]
 
 for num in lista:
         if (num % 2) == 0:
-            print("O número é par")    
+            print(num,"O número é par")    
         else:
-            print("O número é impar")            
+            print(num,"O número é impar")            
 print(lista)
