315ddb27c9544cdc61849a0711ab64d459e049b9
alann085 <142846421+alann085@users.noreply.github.com>
2023-08-22 20:52:18 -0300

    Update atividades2.py

--- Aula 14/atividades2.py
+++ Aula 14/atividades2.py
@@ -1,8 +1,14 @@
 lista = [2,3,1,4,5,6,8,7,9]
 
+qntdpar = 0
+qntdimpar = 0
+
 for num in lista:
         if (num % 2) == 0:
-            print(num,"O número é par")    
+            print(num,"é par")
+            qntdpar += 1
         else:
-            print(num,"O número é impar")            
-print(lista)
+            print(num,"é impar")
+            qntdimpar += 1
+
+print(f"Quantidade de pares: {qntdpar}, quantidade de impares: {qntdimpar}")
